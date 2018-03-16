9df778079db74296d3a8ed113a65eb385448d659
Salmaan Ahmed <salmaanshaik72@gmail.com>
2018-03-16 10:53:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,10 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine','jade');
 app.set('views', path.join(__dirname+'/views'));
 
-app.get('/', function(req,res){
-  res.send('done');
-});
-
 http.listen(3000, function(){
   console.log('server listening on port 3000');
 });
